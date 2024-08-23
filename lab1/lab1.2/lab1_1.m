bd9d80f0260cf9c520b8a00c79c568a8ba768aba
Puy0zz <111022121+Puy0zz@users.noreply.github.com>
2024-08-22 22:17:23 -0500

    Add files via upload

---
+++ lab1/lab1.2/lab1_1.m
@@ -0,0 +1,13 @@
+%%
+plot(out.Temp);
+title("Temperatura contra tiempo para V = 10V");
+xlabel("Tiempo (s)");
+ylabel("Temperatura (°C)")
+%%
+
+plot(out.salida);
+title("Temperatura contra tiempo para V = 5V");
+xlabel("Tiempo (s)");
+ylabel("Temperatura (°C)");
+grid();
+
